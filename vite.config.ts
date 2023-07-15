64689f448b35a9b2a8468926386741056ebf5f54
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-07-15 16:54:04 +1200

    add vite subresouce plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import subResourceIntegrity from "@small-tech/vite-plugin-sri";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), subResourceIntegrity()],
   server: {
     host: "127.0.0.1",
     port: 3000,
